1436216c3f5b4205b02400ff7a5a624368f844a7
Michal Falowski <michal.falowski@uj.edu.pl>
2019-05-20 12:17:19 +0200

    Remove unnecessary imports in docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-from booleanoperator import Release, booleanoperator, run_server
-
+from booleanoperator import Release
 
 # -- Project information -----------------------------------------------------
 
